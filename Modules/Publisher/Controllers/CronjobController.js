1133d85b2b167fe85d42bedb0e1c41328d30cd13
Akshay Sangani <akshay@thetatechnolabs.com>
2020-12-29 10:43:46 +0530

    - Episode image compression updated

--- Modules/Publisher/Controllers/CronjobController.js
+++ Modules/Publisher/Controllers/CronjobController.js
@@ -329,9 +329,9 @@ let PublisherCronjob = {
 
     function updateEpisode(episodeInfo, podcast) {
       return new Promise(async function (resolve, reject) {
-        let dirName = 'uploads/publisher_' + podcast.publisher + '/podcast_' + podcast.podcastId + '/';
-        let fileName = 'episode_img_' + episodeInfo.guid.value;
-        let newImage = (episodeInfo.image && episodeInfo.image.link) ? await imageResize(episodeInfo.image.link, dirName, fileName) : '';
+        // let dirName = 'uploads/publisher_' + podcast.publisher + '/podcast_' + podcast.podcastId + '/';
+        // let fileName = 'episode_img_' + episodeInfo.guid.value;
+        // let newImage = (episodeInfo.image && episodeInfo.image.link) ? await imageResize(episodeInfo.image.link, dirName, fileName) : '';
         EpisodesModel.findOne({"guid": episodeInfo.guid.value, podcast: podcast.id}).then(episodeModel => {
           if(!episodeModel) episodeModel = new EpisodesModel();
           episodeModel.podcast = podcast.id;
@@ -344,7 +344,7 @@ let PublisherCronjob = {
           episodeModel.type = episodeInfo.type;
           episodeModel.length = episodeInfo.length;
           episodeModel.duration = episodeInfo['itunes:duration'].replace(/^(?:00:)?0?/, '');
-          episodeModel.image = newImage;
+          episodeModel.image = (episodeInfo.image && episodeInfo.image.link) ? episodeInfo.image.link : '';
           episodeModel.pubDate = new Date(episodeInfo.pubDate);
           episodeModel.save().then(result => {
             resolve(true);
